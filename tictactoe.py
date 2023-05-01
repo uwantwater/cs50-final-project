24f443da92cc8666f917f1769b40c27b100dd0ae
uwantwater <aradhyadhungelr@gmail.com>
2023-05-01 15:51:10 +0545

    tkinter

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,7 @@
-import arcade
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "WELCOME TO ARCADE"
-
-class tictac(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.BLACK)
+from tkinter import *
 
 def main():
-    game = tictac(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    arcade.run()
+    pass
 
 if __name__ == '__main__':
     main()
